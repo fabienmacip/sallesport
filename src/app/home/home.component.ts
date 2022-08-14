b8552ef3eed45de392edd95ba7030fe92d453495
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-08-14 19:26:12 +0200

    RECETTES, voir toutes les cartes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Recette } from '../interfaces/recette';
+import { RecettesService } from '../services/recettes.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  recettesSubscription!: Subscription;
+  recettes: Recette[] = [];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private recettesService: RecettesService
   ) { }
 
   ngOnInit(): void {
+    this.initRecettes();
   }
 
-  onLogin(): void {
-    this.router.navigate(['/admin','dashboard']);
+  initRecettes(): void {
+    this.recettesSubscription = this.recettesService.recettesSubject.subscribe({
+      next: recettes => this.recettes = recettes,
+      error: console.error
+    });
+    this.recettesService.getRecettes();
+    console.log(this.recettes);
+  }
+
+  ngOnDestroy(): void {
+      this.recettesSubscription.unsubscribe();
   }
 
 }
