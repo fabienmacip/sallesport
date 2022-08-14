c52af92f2c686aa91efbd0f3ba53558f26afc1a1
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-08-14 17:19:24 +0200

    ACCOUNT - Modification de l'adresse mail

--- src/app/account/edit-auth-infos/edit-auth-infos.component.ts
+++ src/app/account/edit-auth-infos/edit-auth-infos.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-edit-auth-infos',
   templateUrl: './edit-auth-infos.component.html',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAuthInfosComponent implements OnInit {
 
-  constructor() { }
+  @Input() currentUser!: User;
+
+  passwordForm!: FormGroup;
+
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
+
 
   ngOnInit(): void {
+    this.initPasswordForm();
   }
 
+  initPasswordForm(): void {
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      newPasswordConfirm: ['', [Validators.required]]
+    })
+  }
 }
--- src/app/account/edit-profile/edit-profile.component.ts
+++ src/app/account/edit-profile/edit-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -13,14 +14,17 @@ export class EditProfileComponent implements OnInit, OnChanges {
   @Input() currentUser!: User;
 
   usernameForm!: FormGroup;
+  emailForm!: FormGroup;
 
   constructor(
     private modalService: NgbModal,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.initUsernameForm();
+    this.initEmailForm();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -30,6 +34,13 @@ export class EditProfileComponent implements OnInit, OnChanges {
   initUsernameForm(): void {
     this.usernameForm = this.formBuilder.group({
       username: ['', [Validators.required]]
+    });
+  }
+
+  initEmailForm(): void {
+    this.emailForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
   }
 
@@ -43,6 +54,26 @@ export class EditProfileComponent implements OnInit, OnChanges {
     .then(() => {
       this.modalService.dismissAll();
     }).catch(console.error);
+  }
+
+  onEditEmail(modal: any): void {
+    this.emailForm.get('email')?.setValue(this.currentUser.email);
+    this.modalService.open(modal, { centered: true});
+  }
+
+  onSubmitEmailForm(): void {
+
+    this.authService.signinUser(<string>this.currentUser.email, this.emailForm.value.password)
+    .then(() => {
+
+      this.currentUser.updateEmail(this.emailForm.value.email)
+      .then(() => {
+        this.modalService.dismissAll();
+        this.emailForm.reset();
+      }).catch(console.error);
+
+    }).catch(console.error);
+
   }
 
 }
