063bb124326cb7895e5b5c4784349baff4dc2761
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-08-15 18:46:08 +0200

    AFFICHE Recettes : toutes si connecté, certaines si visiteur

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { User } from 'firebase/auth';
 import { Recette } from '../interfaces/recette';
 import { RecettesService } from '../services/recettes.service';
+import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,23 +14,34 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   recettesSubscription!: Subscription;
   recettes: Recette[] = [];
+  currentUserSubscription!: Subscription;
+  currentUser!: User;
 
   constructor(
     private router: Router,
-    private recettesService: RecettesService
+    private recettesService: RecettesService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+     this.currentUserSubscription = this.authService.currentUserSubject.subscribe({
+      next: user => this.currentUser = <User>user,
+      error: console.error
+    });
+
     this.initRecettes();
+
   }
 
   initRecettes(): void {
+    //let isVisiteur = true;
     this.recettesSubscription = this.recettesService.recettesSubject.subscribe({
-      next: recettes => this.recettes = recettes,
+      next: recettes => {
+        this.recettes = recettes.filter(el => el.patientsOnlyCheck == false).concat(recettes.filter(el => el.patientsOnlyCheck == true));
+      },
       error: console.error
     });
     this.recettesService.getRecettes();
-    console.log(this.recettes);
   }
 
   ngOnDestroy(): void {
